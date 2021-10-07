2acb873b804c3f0f56b389c7ffff83c364fcde61
xhlove <vvtoolbox.dev@gmail.com>
2021-10-07 10:50:11 +0800

    BaseURL check in Period node

--- XstreamDL_CLI/extractors/dash/childs/baseurl.py
+++ XstreamDL_CLI/extractors/dash/childs/baseurl.py
@@ -4,3 +4,6 @@ from ..mpditem import MPDItem
 class BaseURL(MPDItem):
     def __init__(self, name: str):
         super(BaseURL, self).__init__(name)
+        self.serviceLocation = None # type: str
+        self.dvb_priority = None # type: str
+        self.dvb_weight = None # type: str
