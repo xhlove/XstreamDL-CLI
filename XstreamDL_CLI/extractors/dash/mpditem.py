23a6a12ae0bf814ab26860123e6d73d2ec9ddf41
xhlove <vvtoolbox.dev@gmail.com>
2022-02-25 23:30:08 +0800

    采用全新的时间计算方式 再也不用担心匹配不上了

--- XstreamDL_CLI/extractors/dash/mpditem.py
+++ XstreamDL_CLI/extractors/dash/mpditem.py
@@ -1,6 +1,3 @@
-import re
-
-
 class MPDItem:
     def __init__(self, name: str = "MPDItem"):
         self.name = name
@@ -20,37 +17,54 @@ class MPDItem:
         return [child for child in self.childs if child.name == name]
 
     def match_duration(self, _duration: str) -> float:
+        '''
+        test samples
+        - PT50M0S
+        - PT1H54.600S
+        - PT23M59.972S
+        - P8DT11H6M41.1367016S
+        - P0Y0M0DT0H3M30.000S
+        '''
         if isinstance(_duration, str) is False:
             return
-        duration = re.match(r"PT(\d+)(\.?\d+)S", _duration)
-        if duration is not None:
-            return float(duration.group(1)) if duration else 0.0
-        # PT50M0S
-        duration = re.match(r"PT(\d+)M(\d+)S", _duration)
-        if duration is not None:
-            _m, _s = duration.groups()
-            return int(_m) * 60 + int(_s)
-        # PT1H54.600S
-        duration = re.match(r"PT(\d+)H(\d+)(\.?\d+)S", _duration)
-        if duration is not None:
-            _h, _s, _ss = duration.groups()
-            return int(_h) * 60 * 60 + int(_s) + float("0" + _ss)
-        # PT23M59.972S
-        duration = re.match(r"PT(\d+)M(\d+)(\.?\d+)S", _duration)
-        if duration is not None:
-            _m, _s, _ss = duration.groups()
-            return int(_m) * 60 + int(_s) + float("0" + _ss)
-        # P8DT11H6M41.1367016S
-        duration = re.match(r"P(\d+)DT(\d+)H(\d+)M(\d+)(\.?\d+)S", _duration)
-        if duration is not None:
-            _d, _h, _m, _s, _ss = duration.groups()
-            return int(_d) * 24 * 60 * 60 + int(_h) * 60 * 60 + int(_m) * 60 + int(_s) + float("0" + _ss)
-        # P0Y0M0DT0H3M30.000S
-        duration = re.match(r"PT(\d+)H(\d+)M(\d+)(\.?\d+)S", _duration.replace('0Y0M0D', ''))
-        if duration is not None:
-            _h, _m, _s, _ss = duration.groups()
-            return int(_h) * 60 * 60 + int(_m) * 60 + int(_s) + float("0" + _ss)
-        return 0.0
+
+        def reset_token():
+            nonlocal token_unit, token_time
+            token_unit, token_time = '', ''
+        offset = 0
+        duration = 0.0
+        token_unit = ''
+        token_time = ''
+        t_flag = False
+        while offset < len(_duration):
+            if _duration[offset].isalpha():
+                token_unit += _duration[offset]
+            elif _duration[offset].isdigit() or _duration[offset] == '.':
+                token_time += _duration[offset]
+            else:
+                assert False, f'not possible be here _duration => {_duration}'
+            offset += 1
+            if token_unit == 'P':
+                reset_token()
+            elif token_unit == 'Y' or (t_flag is False and token_unit == 'M'):
+                # 暂时先不计算年和月 有问题再说
+                reset_token()
+            elif token_unit == 'D':
+                duration += 24 * int(token_time) * 60 * 60
+                reset_token()
+            elif token_unit == 'T':
+                t_flag = True
+                reset_token()
+            elif token_unit == 'H':
+                duration += int(token_time) * 60 * 60
+                reset_token()
+            elif token_unit == 'M':
+                duration += int(token_time) * 60
+                reset_token()
+            elif token_unit == 'S':
+                duration += float("0" + token_time)
+                reset_token()
+        return duration
 
     def generate(self):
         pass
