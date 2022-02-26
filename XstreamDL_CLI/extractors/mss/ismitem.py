841fb936798cd6fc8c658e381049cbdc631121b4
xhlove <vvtoolbox.dev@gmail.com>
2022-02-26 09:12:57 +0800

    合并简化代码

--- XstreamDL_CLI/extractors/mss/ismitem.py
+++ XstreamDL_CLI/extractors/mss/ismitem.py
@@ -1,7 +1,7 @@
-import re
+from XstreamDL_CLI.extractors.metaitem import MetaItem
 
 
-class ISMItem(object):
+class ISMItem(MetaItem):
     def __init__(self, name: str = "ISMItem"):
         self.name = name
         self.innertext = ''
@@ -18,27 +18,6 @@ class ISMItem(object):
 
     def find(self, name: str):
         return [child for child in self.childs if child.name == name]
-
-    def match_duration(self, _duration: str) -> float:
-        if isinstance(_duration, str) is False:
-            return
-        duration = re.match(r"PT(\d+)(\.?\d+)S", _duration)
-        if duration is not None:
-            return float(duration.group(1)) if duration else 0.0
-        # PT23M59.972S
-        duration = re.match(r"PT(\d+)M(\d+)(\.?\d+)S", _duration)
-        if duration is not None:
-            _m, _s, _ss = duration.groups()
-            return int(_m) * 60 + int(_s) + float("0" + _ss)
-        # P0Y0M0DT0H3M30.000S
-        duration = re.match(r"PT(\d+)H(\d+)M(\d+)(\.?\d+)S", _duration.replace('0Y0M0D', ''))
-        if duration is not None:
-            _h, _m, _s, _ss = duration.groups()
-            return int(_h) * 60 * 60 + int(_m) * 60 + int(_s) + float("0" + _ss)
-        return 0.0
-
-    def generate(self):
-        pass
 
     def to_int(self, attr_name: str):
         value = self.__getattribute__(attr_name) # type: str
